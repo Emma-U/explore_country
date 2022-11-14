fcad3ba937597335249c413138ab6e1bb72f430f
joseph adariku <102669082+Ushie08@users.noreply.github.com>
2022-11-14 15:12:59 +0100

    create simple utilities

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const kBlack = Colors.black;
+
+const kRed = Colors.red;
+const kWhite = Colors.white;
+const kGreyColor = Color(0xFFE5E5E5);
